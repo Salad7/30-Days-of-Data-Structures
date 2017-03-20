b22b0916f0ac01e334858372b762c04e804b2e91
msalad <msalad@uncc.edu>
2017-03-20 03:33:37 +0000

    Single Linked List

---
+++ SingleLinkedList/Player.h
@@ -0,0 +1,28 @@
+#ifndef Player_H
+#define Player_H
+
+class Player{
+    
+    private:
+        std::string name;
+        int position;
+    public:
+        Player(){
+            
+            
+        }
+        void setName(string s){
+            name = s;
+        }
+        string getName(){
+            return name;
+        }
+    
+    
+    
+}   
+
+
+
+
+#endif
---
+++ SingleLinkedList/linkedlist.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Player.h"
+
+using namespace std;
+
+
+int main(){
+    Player p;
+    
+    p.setName("test");
+    cout << p.getName();
+    
+}   
