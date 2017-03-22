a0fe4aab8362d8f6c8037f5372767f9fe1893546
msalad <msalad@uncc.edu>
2017-03-22 00:03:16 +0000

    Created iterable interface with pure virtual functions

--- Iteration/iterate.cpp
+++ Iteration/iterate.cpp
@@ -3,15 +3,23 @@
 using namespace std;
 
 class iteratableInterface{
-    virtual int test; 
+    public:
+    //Positions to the previous item
+    virtual void previous();
+    //Determines whether the iterator refers to an item returns true if the end has been reached
+    virtual bool isDone();
+    virtual void first();
+    virtual void last();
     
-}
+    
+    
+};
 
-class iteratable{
+class iteratable : public iteratableInterface{
     
     
     
-}
+};
 
 int main(){
     
