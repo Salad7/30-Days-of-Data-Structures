8d74687e7b1f01af29e29073315ebb557ac85ad9
msalad <msalad@uncc.edu>
2017-04-07 19:01:28 +0000

    Finished header file for ItemType

--- SingleLinkedList/UnsortedListADT/ItemType.h
+++ SingleLinkedList/UnsortedListADT/ItemType.h
@@ -1,7 +1,18 @@
 class ItemType {
-public:
-
-
+    enum RelationType{LESS,EQUAL,GREATER};
 private:
+const int MAX_ITEMS = 10;
+RelationType relationType;
 
+
+public:
+RelationType comparedTo(ItemType item);
+void makeEmpty();
+bool isFull();
+int lengthLs();
+ItemType RetrieveItem(ItemType &item, bool &found);
+void insertItem(ItemType item);
+void deleteItem(ItemType item);
+void resetList();
+ItemType getNextItem(ItemType &item);
 };
