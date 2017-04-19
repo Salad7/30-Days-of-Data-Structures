38df964550e5915b352504a509956640fc773206
msalad <msalad@uncc.edu>
2017-04-19 19:07:46 +0000

    added file distro

---
+++ Files/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+//This file is used to train a joystick to remember commands
+//While the x button is held ()
+//Create a seoerate thread to count time
+int main(){
+    
+    
+}
+
+public class Sync {
+syncTiming
+
+
+}
